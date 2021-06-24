b1ff90e249f0aeb3d30b0d5dc39d9934a76c48ac
user156 <user156@springboot-13.alchemycloud.co.in>
2021-06-24 15:03:49 +0530

    collections updated

--- day3/src/collections/AppWithSet.java
+++ day3/src/collections/AppWithSet.java
@@ -1,6 +1,10 @@
 package collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import collections.entity.Emp;
@@ -17,6 +21,34 @@ public class AppWithSet {
 		empSet.add(new Emp(105, "Tirumala", "Hyderabad", 30000));
 		empSet.add(new Emp(106, "Girish", "Hyderabad", 90000));
 		
-		System.out.println(empSet);
+		//find and print all employees who's salary between 30000-60000
+		for(Emp e:empSet) {
+			if(e.getSalary()==30000||e.getSalary()<60000) {
+				System.out.println(e);
+			}
+		}
+		//find all employees who's name contains 'm'
+		for(Emp e:empSet) {
+			if(e.getName().contains("m")) {
+				System.out.println(e.getName());
+			}
+		}
+		
+		//sort the employees based on name in asc order
+		
+		List<Emp> empList=new ArrayList<>(empSet);
+		Collections.sort(empList,new MySetSorter2());
+		
+		System.out.println(empList);
 	}
+}
+
+class MySetSorter2 implements Comparator<Emp>{
+
+	@Override
+	public int compare(Emp e1, Emp e2) {
+		// TODO Auto-generated method stub
+		return e1.getName().compareTo(e2.getName());
+	}
+	
 }
