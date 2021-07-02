213d9a08e5f384eece78186724bd819c0dced989
user156 <user156@springboot-13.alchemycloud.co.in>
2021-07-02 11:24:57 +0530

    spring-jpa

---
+++ jpa-app/src/main/java/com/demo/jpa/entity/Emp.java
@@ -0,0 +1,68 @@
+package com.demo.jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="EMPLOYEE")
+public class Emp {
+	
+	@Id 
+	@Column(name="EMPNO")
+	private int empId;
+	
+	//@Column(name="NAME")
+	private String name;
+	
+	@Column(name="ADDRESS")
+	private String city;
+	
+	//@Column(name="SALARY")
+	private double salary;
+
+	public Emp() {
+
+	}
+
+	public Emp(int empId, String name, String city, double salary) {
+		this.empId = empId;
+		this.name = name;
+		this.city = city;
+		this.salary = salary;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+}
