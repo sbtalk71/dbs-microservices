69ae287798bebf1acd5c12e3748def3e4d18c516
sbtalk71 <sbtalk@gmail.com>
2021-07-09 17:45:30 +0530

    all apps uploaded

---
+++ zuul-gateway/src/main/java/com/demo/zuulgateway/MyZuulRequestInterCeptor.java
@@ -0,0 +1,37 @@
+package com.demo.zuulgateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+
+public class MyZuulRequestInterCeptor extends ZuulFilter {
+
+    @Autowired
+    HttpServletRequest req;
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("The filter working.."+req.getRequestURI());
+        return "Request Data intercepted..";
+    }
+}
